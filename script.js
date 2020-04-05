e16181c5f04883a91285d6e2d68aae9f05661478
Glitch (hello-webpage) <none>
2020-04-05 23:49:23 +0200

    Presque fin

--- script.js
+++ script.js
@@ -12,28 +12,62 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoianVsaWVua29tcCIsImEiOiJjanR1NGFuYjkxMmNvNDBucGI1aXZ4Y285In0.hiSplFD5CODUd9yxRO_qkg'
 }).addTo(mymap);
 
-var marker = L.marker([48.858370, 2.294481]).addTo(mymap);
-marker.bindPopup("<b>This is the Tour Eiffel</b><br>EHEHEHEH").openPopup();
-
-var marker2 = L.marker([48.864824, 2.334595]).addTo(mymap);
-marker2.bindPopup("<b>This is the Louvre</b><br>TMTC LES TABLO").openPopup();
 
 
-async function getData() {
-    let url = "url-de-l-api"
+async function getData(query) {
+    if (query) {
+		url =
+			'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1) ' +
+			query +
+			'&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
+	}
+
+	if (!query) {
+		url =
+			'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type';
+	}
+
+    //let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)" + query + "&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type "
     let response = await fetch(url)
+
     let data = await response.json()
-    data.records.forEach(function(event) {
-    // le titre de l'événement
-    let title = event.fields.title
-    // la latitude
-    let latitude = event.fields.lat_lon[0]
-    // la longitude
-    let longitude = event.fields.lat_lon[1]
-    // on pourrait récupérer d'autres infos..
-    // pour tester, on les affiche dans la console
-    console.log(title + " " + latitude + " " + longitude)
-    // .. mais ce serait mieux de les afficher sur la carte !
+
+    console.log(data);
+
+    data.records.forEach(function (event) {
+        // le titre de l'événement
+        let title = event.fields.title
+
+        let date = event.fields.date_description
+
+        let price = event.fields.price_type
+
+        // si jamais le champs latitude/longitude manque
+        // on ignore cet événement
+        if (!event.fields.lat_lon) {
+            return;
+        }
+
+        // la latitude
+        let latitude = event.fields.lat_lon[0]
+
+        // la longitude
+        let longitude = event.fields.lat_lon[1]
+        // on pourrait récupérer d'autres infos..
+
+        // pour tester, on les affiche dans la console
+        console.log(title + " " + latitude + " " + longitude)
+
+        // .. mais ce serait mieux de les afficher sur la carte !
+        var marker = L.marker([latitude, longitude]).addTo(mymap);
+        marker.bindPopup(title + "<br><br>" + date + "<br>" + price).openPopup();
     })
-    }
-    getData()
+}
+
+getData()
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    getData(searchInput.value);
+}
